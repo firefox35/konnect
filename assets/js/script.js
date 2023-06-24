b60cbebb76602fb61e8681b1aff1d8ca2da1cfaf
firefox35 <f1firefox@yahoo.ie>
2023-06-24 16:01:10 +0000

    Add array and splitting array to startGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,20 +22,56 @@ function storeInfo() {
 }
 
 
+
+
+
 function timer() {
 
 }
 
 
+
 function startGame() {
+    //Setting an Array of Words
+    let array = ["brown", "truck", "train"];
+    let guess = inputGuess();
+    //Choosing a Random word from the Array
+    let word = array[Math.floor(Math.random() * array.length)];
+
+    console.log(word);
+    console.log(guess);
+    //Split the Array in Seperate Letters
+    let char = word.split('');
+    let checkGuess = guess.split('');
+
+    let result = char.toLocaleString() === checkGuess.toLocaleString();
+    if (result) {
+        console.log((char).style.color = "green");
+    } else {
+        console.log('Its wrong');
+    }
+
+
+    console.log(char);
+    console.log(checkGuess);
+
+
+
+
 
 }
+
+startGame();
 
 function game() {
 
 }
 
 function inputGuess() {
+    let userGuess;
+    userGuess = document.getElementById('enterBox').value = "";
+    document.getElementsByName('guess').innerHTML = (userGuess);
+    alert(userGuess);
 
 }
 
